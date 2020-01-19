a62f0821811f4759fd52f8f3fb8e70733578df40
Sashank Gogula <sashankg@gmail.com>
2020-01-19 14:06:09 -0500

    Added currentStory reducer

---
+++ src/reducers/currentStory.js
@@ -0,0 +1,13 @@
+export default function currentStory(state, action) {
+  switch(action.type) {
+    case 'START_STORY':
+      return {
+        id: action.id,
+        page: 1,
+      }
+    default:
+      return {
+        id: null,
+      }
+  }
+}
