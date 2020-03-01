12abde4364afb96eaa21fb8fc642141af6e48df1
jessicaxiao11 <jessicaxiao11@gmail.com>
2020-03-01 14:13:56 -0500

    began implementing findBlanks function, should be more generalized

--- src/actions/storyActions.js
+++ src/actions/storyActions.js
@@ -1,5 +1,28 @@
 import story1 from '../data/story1.json'
 
+//generalize function
+function findBlanks(acc, pages) {
+    for (var i = 0; i < pages.length; i++) {
+        var currPage = page[i];
+        const entries = Object.entries(currPage)
+        for (const entry of entries) {
+            const key = entry[0]
+            const value = entry[1]
+            //base case
+            if (!Array.isArray(value)) {
+                //returns list of blanks
+                var blanks = parseString(value)
+                acc = acc.concat(blanks)
+            }
+            //recursive case
+            else {
+                //incomplete
+            }
+        }
+    
+    }
+}
+
 export function loadStories() {
     return function(dispatch) {
         const stories = [story1]
